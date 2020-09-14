d23adf8786db5e0513d6941cad25d47ed96715e7
Phil-Longhurst <59968417+Phil-Longhurst@users.noreply.github.com>
2020-09-14 11:20:14 +0100

    a lot of chnges
    go go og

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 // Nav bar //
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-        $('.nav').addClass('affix');
-        console.log("OK");
-    } else {
-        $('.nav').removeClass('affix');
-    }
-});
+// $(window).scroll(function() {
+//     if ($(document).scrollTop() > 50) {
+//         $('.nav').addClass('affix');
+//         console.log("OK");
+//     } else {
+//         $('.nav').removeClass('affix');
+//     }
+// });
 
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+// $('.navTrigger').click(function () {
+//     $(this).toggleClass('active');
+//     console.log("Clicked menu");
+//     $("#mainListDiv").toggleClass("show_list");
+//     $("#mainListDiv").fadeIn();
 
-});
+// });
+
+let message = 'heloooo'
+var pContent = document.getElementById('js')
+
+pContent.innerHTML =
+    '<span>' + message + '</span>'
+
+
+
+
 
